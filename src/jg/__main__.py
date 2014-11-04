793be99b5a033959fdb5215d123bf041f4036ae1
Jānis Ābele <janis.abele@gmail.com>
2014-11-04 12:46:36 +0200

    Add graph generation for simple cases

--- src/jg/__main__.py
+++ src/jg/__main__.py
@@ -1,5 +1,13 @@
+import re
 import sys
 
+import py
+from graphviz import Digraph
+from itertools import chain
+
+
+EXTENDS_RE = re.compile(r'{%\s*extends\s*[\'"](.*)[\'"]\s*%}')
+INCLUDE_RE = re.compile(r'{%\s*include\s*[\'"](.*)[\'"]\s*%}')
 
 def main(argv=()):
     """
@@ -14,6 +22,30 @@ def main(argv=()):
 
     print(argv)
     return 0
+
+
+def generate_template_graph(root_path):
+    cwd = py.path.local(root_path)
+    assert cwd == '/home/abele/Devel/jinja-graph/tests/fixtures'
+    template_path_list = py.path.local(root_path).listdir(
+        fil=lambda p: p.isfile()
+    )
+    template_dict = {path.strpath.replace(cwd.strpath, '', 1).lstrip('/'): {'path': path} for path in template_path_list}
+    dot = Digraph()
+
+    for rel_path in template_dict:
+        dot.node(rel_path)
+
+        file_content = template_dict[rel_path]['path'].read()
+        derived_file_seq = chain(
+            EXTENDS_RE.findall(file_content),
+            INCLUDE_RE.findall(file_content)
+        )
+        for derived_file_path in derived_file_seq:
+            dot.edge(rel_path, derived_file_path)
+
+    return dot
+
 
 if __name__ == "__main__":
     sys.exit(main())
--- tests/test_jg.py
+++ tests/test_jg.py
@@ -1,4 +1,19 @@
-from jg.__main__ import main
+from jg.__main__ import main, generate_template_graph
 
-def test_main():
+
+def test_main_returns_0_on_successful_execution():
     assert main([]) == 0
+
+
+def test_returns_graphviz_graph_for_fixture_directory():
+    dot = generate_template_graph(root_path='./tests/fixtures')
+
+    assert dot.source == ('digraph {\n\t"index.html"\n'
+                          '\t\t"index.html" -> "header.html"\n'
+                          '\t\t"index.html" -> "footer.html"\n'
+                          '\t"analytics.html"\n'
+                          '\t"custom_index.html"\n'
+                          '\t\t"custom_index.html" -> "index.html"\n'
+                          '\t\t"custom_index.html" -> "snippets/ga.html"\n'
+                          '\t"footer.html"\n'
+                          '\t"header.html"\n}')
