890cfd7ea280502e2838d30396c2571f2c08a230
Justin Schuhmann <jmschu02@gmail.com>
2023-07-10 14:01:58 -0400

    Updated to support all file types supports import usage supports reversing the node and key pairs supports printing the top 10 result

--- src/jg/__main__.py
+++ src/jg/__main__.py
@@ -9,7 +9,8 @@ from graphviz import Digraph
 
 EXTENDS_RE = re.compile(r'{%\s*extends\s*[\'"](.*)[\'"]\s*%}')
 INCLUDE_RE = re.compile(r'{%\s*include\s*[\'"](.*)[\'"]\s*%}')
-TEMPLATE_PATTERN = '*.html'
+IMPORT_RE = re.compile(r'{%-?\s*import\s*[\'"](.*)[\'"]\s*')
+SECOND_IMPORT_RE = re.compile(r'{%-?\s*from\s*[\'"](.*)[\'"]\s*import\s*')
 
 
 def main(argv=()):
@@ -23,22 +24,37 @@ def main(argv=()):
     Does stuff.
     """
     parser = argparse.ArgumentParser(
-        description=u'Generate Jinja template dependency graph')
+        description="Generate Jinja template dependency graph"
+    )
+    parser.add_argument("-i", "--input", default="./", help="Root path", required=True)
     parser.add_argument(
-        'i',
-        default='./',
-        help=u'Root path',
+        "-o", "--output", default="./output.dot", help="Output filename", required=True
     )
     parser.add_argument(
-        'o',
-        default='./output.dot',
-        help=u'Output filename',
+        "-e",
+        "--extension",
+        default=".html",
+        help="The extension to search for dependency tree, examples: .html, .j2, .css, .txt",
     )
-    args = parser.parse_args(argv or sys.argv[1:])
-    root_path = args.i
-    output_filename = args.o
+    parser.add_argument(
+        "-r", "--reverse", action="store_true", help="Reverse the graph"
+    )
+    parser.add_argument(
+        "-p",
+        "--print",
+        action="store_true",
+        help="Print the TOP 10 referenced items"
+    )
 
-    dot = generate_template_graph(root_path=root_path)
+    args = parser.parse_args(argv or sys.argv[1:])
+    root_path = args.input
+    output_filename = args.output
+    TEMPLATE_PATTERN = f"*{args.extension}"
+
+    dot = generate_template_graph(
+        root_path=root_path, template_pattern=TEMPLATE_PATTERN, reverse=args.reverse, print_top_10=args.print
+    )
+
     write(dot.source, output_filename)
 
     return 0
@@ -48,31 +64,53 @@ def write(content, output_filename):
     py.path.local(output_filename).write(content, ensure=True)
 
 
-def generate_template_graph(root_path):
+def generate_template_graph(root_path, template_pattern: str, reverse: bool = False, print_top_10: bool = False):
     cwd = py.path.local(root_path)
-    node_name_and_template_path = ((_node_name(path, cwd), path)
-                                   for path in _template_path_seq(root_path))
-    dot = Digraph()
+    node_name_and_template_path = (
+        (_node_name(path, cwd), path)
+        for path in _template_path_seq(root_path, template_pattern=template_pattern)
+    )
+    dictionary = {}
 
     for node_name, template_path in node_name_and_template_path:
-        dot.node(node_name)
+        if not reverse and dictionary.get(node_name) is None:
+            dictionary[node_name] = []
         file_content = template_path.read()
+
         derived_file_seq = chain(
             EXTENDS_RE.findall(file_content),
-            INCLUDE_RE.findall(file_content)
+            INCLUDE_RE.findall(file_content),
+            IMPORT_RE.findall(file_content),
+            SECOND_IMPORT_RE.findall(file_content),
         )
         for derived_file_path in derived_file_seq:
-            dot.edge(node_name, derived_file_path)
+            if reverse:
+                if dictionary.get(derived_file_path) is None:
+                    dictionary[derived_file_path] = []
+
+                dictionary[derived_file_path].append({"source": derived_file_path, "target": node_name})
+            else:
+                dictionary[node_name].append({"source": node_name, "target": derived_file_path})
+
+    dot = Digraph()
+
+    for key in dictionary.keys():
+        dot.node(key)
+        for item in dictionary.get(key):
+            dot.edge(item.get('source'), item.get('target'))
+
+    if print_top_10:
+        print(' '.join(sorted(dictionary, key=lambda k: len(dictionary[k]), reverse=True)[:10]))
 
     return dot
 
 
 def _node_name(path, cwd):
-    return path.strpath.replace(cwd.strpath, '', 1).lstrip('/')
+    return path.strpath.replace(cwd.strpath, "", 1).lstrip("/")
 
 
-def _template_path_seq(root_path):
-    return py.path.local(root_path).visit(TEMPLATE_PATTERN)
+def _template_path_seq(root_path, template_pattern: str):
+    return py.path.local(root_path).visit(template_pattern)
 
 
 if __name__ == "__main__":
